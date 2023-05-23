6c088eabd25d3d2985f91a7a74fa4da814af127f
Jose Aguilar <jose.aguilar@casamba.net>
2023-05-23 09:11:37 -0700

    Add project files.

---
+++ UdemyRestSharpNetCore/UdemyRestSharpNetCore/Steps/GetPostsSteps.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using RestSharp;
+using UdemyRestSharpNetCore.Base;
+using UdemyRestSharpNetCore.Model;
+using UdemyRestSharpNetCore.Utilities;
+using System.Threading;
+using TechTalk.SpecFlow;
+using Snapshooter.NUnit;
+
+namespace UdemyRestSharpNetCore.Steps
+{
+
+    [Binding]
+    public class GetPostsSteps
+    {
+        private Settings _settings;
+        public GetPostsSteps(Settings settings) => _settings = settings;
+
+
+        [Given(@"I perform GET operation for ""(.*)""")]
+        public void GivenIPerformGETOperationFor(string url)
+        {
+            _settings.Request = new RestRequest(url, Method.GET);
+        }
+
+        [Given(@"I perform operation for post ""(.*)""")]
+        public void GivenIPerformOperationForPost(int postId)
+        {
+            Thread.Sleep(2000);
+            _settings.Request.AddUrlSegment("postid", postId.ToString());
+            _settings.Response = _settings.RestClient.ExecuteAsyncRequests<Posts>(_settings.Request).GetAwaiter().GetResult();
+        }
+
+        [Then(@"I should see the ""(.*)"" name as ""(.*)""")]
+        public void ThenIShouldSeeTheNameAs(string key, string value)
+        {
+            //Creating a snapshot, validates 
+            Snapshot.Match(_settings.Response.Content, matchOption => matchOption.IgnoreField("title"));
+            Assert.That(_settings.Response.GetResponseObject(key), Is.EqualTo(value), $"The {key} is not matching");
+        }
+
+    }
+}
---
+++ UdemyRestSharpNetCore/UdemyRestSharpNetCore/Steps/PostProfileSteps.cs
@@ -0,0 +1,37 @@
+﻿using RestSharp;
+using UdemyRestSharpNetCore.Base;
+using UdemyRestSharpNetCore.Model;
+using UdemyRestSharpNetCore.Utilities;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+
+namespace UdemyRestSharpNetCore.Steps
+{
+
+    [Binding]
+    public class PostProfileSteps
+    {
+        private Settings _settings;
+        public PostProfileSteps(Settings settings) => _settings = settings;
+
+        [Given(@"I Perform POST operation for ""(.*)"" with body")]
+        public void GivenIPerformPOSTOperationForWithBody(string url, Table table)
+        {
+
+            dynamic data = table.CreateDynamicInstance();
+
+            _settings.Request = new RestRequest(url, Method.POST);
+
+            //_settings.Request.RequestFormat = DataFormat.Json;
+            //_settings.Request.AddBody(new { name = data.name.ToString() });
+
+            _settings.Request.AddJsonBody(new { name = data.name.ToString() });
+
+            _settings.Request.AddUrlSegment("profileNo", ((int)data.profile).ToString());
+
+            _settings.Response = _settings.RestClient.ExecuteAsyncRequests<Posts>(_settings.Request).GetAwaiter().GetResult();
+
+        }
+
+    }
+}
